11fdffae05667e5bc75652b18c0c3f8a4eae45a3
SHAMANTH D A <88526736+SHAMANTHDA@users.noreply.github.com>
2023-01-15 13:29:26 +0530

    scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rgbd2d;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0) //it is positive
+        {
+            rgbd2d.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0) //it is negative
+        {
+            rgbd2d.velocity = new Vector2(-speed, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0) //it is positive
+        {
+            rgbd2d.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0) //it is negative
+        {
+            rgbd2d.velocity = new Vector2(0f, -speed);
+        }
+        else if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            //stop
+            rgbd2d.velocity = new Vector2(0f, 0f); 
+        }
+    }
+}
