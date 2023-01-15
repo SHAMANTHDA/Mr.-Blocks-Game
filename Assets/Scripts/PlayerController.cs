55d3b8d5243307e3808ca305670272344317cc35
SHAMANTH D A <88526736+SHAMANTHDA@users.noreply.github.com>
2023-01-15 14:01:59 +0530

    new

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rgbd2d;
     public float speed;
-   
+    public float acceleration = 2.0f;
+    public float maxspeed = 60f;
+    private float currentspeed = 0f;
 
     void Start()
     {
@@ -16,21 +18,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if(Input.GetAxis("Horizontal") > 0) //it is positive
         {
             rgbd2d.velocity = new Vector2(speed, 0f);
+            transform.Translate(Vector3.forward * currentspeed * Time.deltaTime);
+            currentspeed += acceleration * Time.deltaTime;
         }
         else if (Input.GetAxis("Horizontal") < 0) //it is negative
         {
             rgbd2d.velocity = new Vector2(-speed, 0f);
+            transform.Translate(Vector3.forward * currentspeed * Time.deltaTime);
+            currentspeed += acceleration * Time.deltaTime;
         }
         else if (Input.GetAxis("Vertical") > 0) //it is positive
         {
             rgbd2d.velocity = new Vector2(0f, speed);
+            transform.Translate(Vector3.forward * currentspeed * Time.deltaTime);
+            currentspeed += acceleration * Time.deltaTime;
         }
         else if (Input.GetAxis("Vertical") < 0) //it is negative
         {
             rgbd2d.velocity = new Vector2(0f, -speed);
+            transform.Translate(Vector3.forward * currentspeed * Time.deltaTime);
+            currentspeed += acceleration * Time.deltaTime;
         }
         else if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
         {
